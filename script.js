972ffb1d07d9b8e51067be1c46f1e7e4358cfb8f
Daevanion <166545774+Daevanion@users.noreply.github.com>
2025-05-12 13:46:24 +0300

    updated extra javascripts and email funcationality using Emailjs

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+
+function sendMail(){
+    let pars ={
+        email: document.getElementById('email_input').value
+    }
+
+    emailjs.send("service_mg5zvi1","template_gcm39gm",pars).then(alert("Email sent!"))
+}
